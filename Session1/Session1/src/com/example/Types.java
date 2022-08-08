30be2892d0f65d86e92839e0eebca54442f16c38
OscarTalero <talero@gmail.com>
2022-08-08 08:17:02 -0500

    Session 1

---
+++ Session1/Session1/src/com/example/Types.java
@@ -0,0 +1,39 @@
+package com.example;
+
+public class Types {
+
+    public static void main(String[] args) {
+
+        // 1. Numerics
+
+        // 1.1 Integers
+        byte var1 = 3;
+        short var2 = 40;
+        int var3 = 5000;
+        long var4 = 6000000;
+
+        // 1.2 Decimals
+        float var5 = 4.3f;
+        double var6 = 14.6d;
+
+        // 2. Booleans
+        boolean var7 = true;
+        boolean var8 = false;
+
+        // 3. Text
+        char var9 = 'a';
+        String var10 = "This is a text";
+
+        System.out.println("this is a date type byte: " + var1);
+        System.out.println("this is a date type short: " + var2);
+        System.out.println("this is a date type int: " + var3);
+        System.out.println("this is a date type long: " + var4);
+        System.out.println("this is a date type float: " + var5);
+        System.out.println("this is a date type double: " + var6);
+        System.out.println("this is a date type boolean: " + var7);
+        System.out.println("this is a date type boolean: " + var8);
+        System.out.println("this is a date type char: " + var9);
+        System.out.println("this is a date type String: " + var10);
+
+    }
+}
