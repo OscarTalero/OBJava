f0458c6f03e41e574f73d285a72636ec8e12e3da
OscarTalero <talero@gmail.com>
2022-08-08 22:18:46 -0500

    Session2

---
+++ Session1/src/com/example/Functions.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class Functions {
+
+    public static void main(String[] args) {
+
+        int precio = 100;
+        System.out.println("El valor total es: " + valorTotal(precio));
+    }
+
+    public static float valorTotal (int precio) {
+
+        float total;
+        float iva = 0.16f;
+        return total = precio + (precio * iva);
+
+    }
+}
